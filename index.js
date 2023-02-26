8a623ba22d3d7a178158afe919f199566e4a9acc
Lakiyas <86748654+Lakiyas@users.noreply.github.com>
2023-02-26 13:19:37 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ app.get('/', (req, res) => {
   res.send('Hello, world!');
 });
 
-app.listen(process.env.PORT || 80, () => {
+app.listen(process.env.PORT || 80, '0.0.0.0', () => {
   console.log('App listening on port 80!');
 });
