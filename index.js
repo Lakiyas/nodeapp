3da27e58ebcedc6ffd113c99c3bdd9b71e01c759
Lakiyas <86748654+Lakiyas@users.noreply.github.com>
2023-02-26 03:13:27 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('Hello, world!');
 });
 
-app.listen(3000, () => {
-  console.log('Server started on port 3000');
+app.listen(process.env.PORT || 3000, () => {
+  console.log('App listening on port 3000!');
 });
